aafd44ef2ad650134d2a4612a01ea35043bfc31f
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2023-05-19 09:00:38 +0200

    handle validation errors

--- data-producer/src/main/java/com/quetzalcoatl/microservices/dataproducer/controller/ExceptionHandlerController.java
+++ data-producer/src/main/java/com/quetzalcoatl/microservices/dataproducer/controller/ExceptionHandlerController.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.invoke.MethodHandles;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
@@ -25,5 +27,23 @@ public class ExceptionHandlerController {
                 "Message from sensor [%s] can't be read. Cause: [%s]. Details:[%s]",
                 sensorId, error.getClass().getSimpleName(), error.getCause().getLocalizedMessage()
                 ));
+    }
+
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public void handleValidation(HttpServletRequest request, MethodArgumentNotValidException error){
+        String sensorIdHeader = request.getHeader(Controller.SENSOR_ID_HEADER);
+        String sensorId = sensorIdHeader == null? "N/A" : sensorIdHeader;
+
+        String result = error.getBindingResult().getFieldErrors().stream()
+                .filter(fe -> fe.getDefaultMessage() != null)
+                .map(fe -> fe.getDefaultMessage().startsWith(fe.getField()) ?
+                        fe.getDefaultMessage() : fe.getField() + ": " + fe.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        logger.error(String.format(
+                "Data from sensor [%s] has a validation issue: [%s]",
+                sensorId, result
+        ));
     }
 }
