70946156e221b2635c4950f8c7943cbda25fa5ac
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2023-05-24 12:18:58 +0200

    convert DTO and send to Kafka

--- data-producer/src/main/java/com/quetzalcoatl/microservices/dataproducer/controller/Controller.java
+++ data-producer/src/main/java/com/quetzalcoatl/microservices/dataproducer/controller/Controller.java
@@ -1,11 +1,12 @@
 package com.quetzalcoatl.microservices.dataproducer.controller;
 
 import com.quetzalcoatl.microservices.dataproducer.ProducerService;
+import com.quetzalcoatl.microservices.dataproducer.model.BlackHoleData;
 import com.quetzalcoatl.microservices.dataproducer.model.BlackHoleDataDTO;
+import com.quetzalcoatl.microservices.dataproducer.util.BlackHoleDataMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -26,14 +27,17 @@ public class Controller {
     }
 
     @PostMapping(value = "/postData", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addData(@Valid @RequestBody BlackHoleDataDTO dto, @RequestHeader(name = SENSOR_ID_HEADER) String sensorId, BindingResult bindingResult){
-        if(!Objects.equals(sensorId, dto.getSensorId().name())){
+    public void addData(@Valid @RequestBody BlackHoleDataDTO dto, @RequestHeader(name = SENSOR_ID_HEADER) String sensorId) {
+        if (!Objects.equals(sensorId, dto.getSensorId().name())) {
             logger.error(
                     "Sensor Id from header and body are different (header:[{}], body:[{}]). Skipping this data...",
                     sensorId, dto.getSensorId().name());
             return;
         }
         logger.info(dto.toString());
+        BlackHoleData blackHoleData = BlackHoleDataMapper.convertFromDto(dto);
+        logger.info(blackHoleData.toString());
+        producerService.publishData(blackHoleData);
     }
 
 }
---
+++ data-producer/src/main/java/com/quetzalcoatl/microservices/dataproducer/util/BlackHoleDataMapper.java
@@ -0,0 +1,22 @@
+package com.quetzalcoatl.microservices.dataproducer.util;
+
+import com.quetzalcoatl.microservices.dataproducer.model.BlackHoleData;
+import com.quetzalcoatl.microservices.dataproducer.model.BlackHoleDataDTO;
+
+import java.util.UUID;
+
+public final class BlackHoleDataMapper {
+    private BlackHoleDataMapper(){
+
+    }
+    public static BlackHoleData convertFromDto(BlackHoleDataDTO dto){
+        BlackHoleData data = new BlackHoleData();
+        data.setDataId(UUID.randomUUID());
+        data.setSensorId(dto.getSensorId());
+        data.setAccretionRate(dto.getAccretionRate());
+        data.setXRayEmission(dto.getEmission());
+        data.setGravitationalWavesDensity(dto.getGravitationalWavesDensity());
+        data.setEventHorizonStability(dto.getEventHorizonStability());
+        return data;
+    }
+}
