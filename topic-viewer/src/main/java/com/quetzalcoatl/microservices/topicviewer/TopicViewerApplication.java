32fbef6738092b781dce87fffaa27727942d145c
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2023-06-26 18:27:45 +0200

    add service template for viewing kafka topics

---
+++ topic-viewer/src/main/java/com/quetzalcoatl/microservices/topicviewer/TopicViewerApplication.java
@@ -0,0 +1,13 @@
+package com.quetzalcoatl.microservices.topicviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicViewerApplication.class, args);
+	}
+
+}
---
+++ topic-viewer/src/test/java/com/quetzalcoatl/microservices/topicviewer/TopicViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quetzalcoatl.microservices.topicviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
