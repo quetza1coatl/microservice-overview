35160878896577b6c91ba6f8ba99619cf43a9643
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2023-09-25 17:57:10 +0200

    add schema registry to topicviewer

--- topic-viewer/src/main/java/com/quetzalcoatl/microservices/topicviewer/kafka/KafkaConsumerConfig.java
+++ topic-viewer/src/main/java/com/quetzalcoatl/microservices/topicviewer/kafka/KafkaConsumerConfig.java
@@ -1,12 +1,13 @@
 package com.quetzalcoatl.microservices.topicviewer.kafka;
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.UUIDDeserializer;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+
 
 
 import java.util.*;
@@ -16,12 +17,16 @@ public class KafkaConsumerConfig {
     @Value("${SPRING_KAFKA_BOOTSTRAP-SERVERS}")
     private String bootstrapAddress;
 
+    @Value("${SCHEMA_REGISTRY_URL}")
+    private String schemaRegistryUrl;
+
     @Bean("generalConsumerConfigs")
     public Properties generalConsumerConfigs(){
         Properties config = new Properties();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        config.put("schema.registry.url", schemaRegistryUrl);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UUIDDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return config;
